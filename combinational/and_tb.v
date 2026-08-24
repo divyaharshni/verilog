560da5d9fc0fa1c79dc20f0a1c14cca2ffa3d0b5
divyaharshni <kdivyaharshini@gmail.com>
2026-08-24 23:38:42 +0530

    Update and_tb.v

--- combinational/and_tb.v
+++ combinational/and_tb.v
@@ -1,1 +1,23 @@
-
+module and_gate_tb;
+reg a, b;
+wire c;
+and_gate uut(a,b,c);
+initial begin
+a = 0;
+b = 0;
+#10;
+a = 0;
+b = 1;
+#10;
+a = 1;
+b = 0;
+#10;
+a = 1;
+b = 1;
+#10;
+$finish;
+end
+initial begin
+$monitor("Time=%0t A=%b B=%b C=%b",$time,a,b,c);
+end
+endmodule
