76d0eea4ca840eaf792be8dfad9da53be6877020
divyaharshni <kdivyaharshini@gmail.com>
2026-08-25 18:17:05 +0530

    Add testbench for D Flip-Flop module

--- sequential/dflipflop/dflipflop_tb.v
+++ sequential/dflipflop/dflipflop_tb.v
@@ -1,1 +1,32 @@
-
+module dflipflop_tb;
+reg d,clk,rst;
+wire q,qbar;
+dflipflop uut(.q(q),.qbar(qbar),.d(d),.clk(clk),.rst(rst));
+always #5 clk = ~clk;
+initial begin
+clk = 0;
+rst = 0;
+d = 0;
+#10;
+rst = 1;
+#10;
+d = 0;
+#10;
+d = 1;
+#10;
+d = 0;
+#10;
+d = 1;
+#10;
+d = 1;
+#10;
+rst = 0;
+#10;
+rst = 1;
+#10;
+$finish;
+end
+initial begin
+$monitor("Time=%0t|rst=%b|clk=%b|d=%b|q=%b|qbar=%b",$time,rst,clk,d,q,qbar);
+end
+endmodule
