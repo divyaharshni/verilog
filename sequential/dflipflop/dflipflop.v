606163551ef1afbbcd4bee1047b0bcf2dbd1b641
divyaharshni <kdivyaharshini@gmail.com>
2026-08-25 18:16:22 +0530

    Update dflipflop.v

--- sequential/dflipflop/dflipflop.v
+++ sequential/dflipflop/dflipflop.v
@@ -1,1 +1,16 @@
-
+module dflipflop(q,qbar,d,clk,rst);
+output reg q,qbar;
+input d,clk,rst;
+always @(posedge clk or negedge rst)
+begin
+if(!rst)begin
+q<=0;
+qbar<=1;
+end
+else
+begin
+q<=d;
+qbar<=~d;
+end
+end
+endmodule
