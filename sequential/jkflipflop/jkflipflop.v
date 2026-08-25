7adb37145a40399fe603d922cc01dd82fc7b24f6
divyaharshni <kdivyaharshini@gmail.com>
2026-08-25 17:52:52 +0530

    Update jkflipflop.v

--- sequential/jkflipflop/jkflipflop.v
+++ sequential/jkflipflop/jkflipflop.v
@@ -1,1 +1,32 @@
-
+module jkflipflop(q,qbar,j,k,clk,rst);
+output reg q,qbar;
+input j,k,clk,rst;
+always @(posedge clk or negedge rst)
+begin
+if (!rst) begin
+q<=0;
+qbar<=1;
+end
+else
+begin
+case ({j,k})
+2'b00 :begin
+q<=q;
+qbar<=qbar;
+end 
+2'b01:begin
+q<=0;
+qbar<=1;
+end 
+2'b10:begin
+q<=1;
+qbar<=0;
+end 
+2'b11:begin
+q<=qbar;
+qbar<=q;
+end
+endcase
+end
+end
+endmodule
