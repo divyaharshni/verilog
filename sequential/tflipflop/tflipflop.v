e6021d54d77d194bb09a6f378b85ba538cb66ce7
divyaharshni <kdivyaharshini@gmail.com>
2026-08-25 18:26:36 +0530

    Update tflipflop.v

--- sequential/tflipflop/tflipflop.v
+++ sequential/tflipflop/tflipflop.v
@@ -1,1 +1,24 @@
-
+module tflipflop(q,qbar,t,clk,rst);
+output reg q,qbar;
+input t,clk,rst;
+always @(posedge clk or negedge rst)
+begin
+if(!rst)begin
+q<=0;
+qbar<=1;
+end
+else
+begin
+case (t)
+1'b0:begin
+q<=q;
+qbar<=qbar;
+end
+1'b1:begin
+q<=qbar;
+qbar<=q;
+end
+endcase
+end
+end
+endmodule
