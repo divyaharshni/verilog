ecb5ccca192a56b8569047120f337c76deba1b50
divyaharshni <kdivyaharshini@gmail.com>
2026-09-04 16:26:26 +0530

    Add testbench for XNOR gate

---
+++ combinational/xnor/xnor_tb.v
@@ -0,0 +1,23 @@
+module xnor_gate_tb;
+reg a,b;
+wire c;
+xnor_gate uut(a,b,c);
+initial begin
+a = 0;
+b = 0;                                                                                           
+#10;                                                                                                  
+a = 0;
+b = 1;                                                                                          
+#10;
+a = 1;
+b = 0;
+#10;
+a = 1;
+b = 1;                                                                                                      
+#10;
+$finish;
+end
+Initial begin
+$monitor("Time=%0t A=%b B=%b C=%b",$time,a,b,c);
+end
+endmodule
